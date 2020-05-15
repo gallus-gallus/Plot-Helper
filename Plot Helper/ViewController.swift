033a043fd7c1d6f512fb4813d404908d2f19471b
Anyll <anyllmarkevich@gmail.com>
2020-05-15 15:54:39 -0600

    Added support for fractional vases as cheatgrass input.

--- Plot Helper/ViewController.swift
+++ Plot Helper/ViewController.swift
@@ -14,7 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var plot: UISegmentedControl!
+    @IBAction func plotChanged(_ sender: Any) {
+    }
+    @IBOutlet weak var direction: UISegmentedControl!
+    @IBAction func directionChanged(_ sender: Any) {
+    }
+    @IBOutlet weak var distance: UISegmentedControl!
+    @IBAction func distanceChanged(_ sender: Any) {
+    }
+    @IBOutlet weak var cover: UISegmentedControl!
+    @IBAction func coverChanged(_ sender: Any) {
+    }
+    @IBOutlet weak var input: UITextField!
+    @IBAction func inputEdited(_ sender: Any) {
+    }
+    @IBOutlet weak var autoSwitch: UISwitch!
+    @IBAction func autoSwitchChanged(_ sender: Any) {
+    }
+    @IBOutlet weak var fractional: UISegmentedControl!
+    @IBAction func fractionalChanged(_ sender: Any) {
+    }
+    @IBOutlet weak var addEdit: UIButton!
+    @IBAction func addEditPressed(_ sender: Any) {
+    }
 }
 
