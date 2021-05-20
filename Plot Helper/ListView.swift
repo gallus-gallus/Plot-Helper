3a293aafa4ad8e437bce5f8ea10d3b4b767cf1d7
Anyll Markevich <anyllmarkevich@gmail.com>
2021-05-20 10:34:04 -0600

    Contents should now work in the list view, but not in the export view. This has not been tested.

--- Plot Helper/ListView.swift
+++ Plot Helper/ListView.swift
@@ -15,15 +15,21 @@ class ListView: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        //Sorting occcurs here.
         var masterSting = ""
         var masterList = [storage.sample]()
+        var masterContentList = [storage.contents]()
         figureSamples()
         for plot in 1...8{
             for direction in 1...8{
                 for distance in 1...5{
                     let sampleExitsNum: Int? = sampleExists(at: storage.sample(plot: plot, direction: direction, distance: distance, coverCheatgrass: 0, coverCheatgrassLitter: 0, coverGround: 0, coverLitter: 0, coverPlant: 0))
+                    let contentExistsNum = contentExists(at: storage.contents(plot: plot, direction: direction, distance: distance, contents: storage.contentTypes.Default))
                     if sampleExitsNum != nil{
                         masterList.append(storage.listOfSamples[sampleExitsNum!])
+                    }
+                    if contentExistsNum != nil{
+                        masterContentList.append(storage.listOfContents[contentExistsNum!])
                     }
                 }
             }
@@ -32,12 +38,31 @@ class ListView: UIViewController {
         for i in masterList{
             masterSting += sampleToGoodString(sample: i)
         }
+        
         for _ in 1...5{
             masterSting += "\n"
         }
+        
+        for i in masterContentList{
+            masterSting += contentToGoodString(content: i)
+        }
+        
+        for _ in 1...5{
+            masterSting += "\n"
+        }
+        
         for i in masterList{
             CSVSting += sampleToCSVString(sample: i)
         }
+        
+        for _ in 1...5{
+            CSVSting += "\n"
+        }
+        
+        for i in masterContentList{
+            CSVSting += contentToCSVString(content: i)
+        }
+        
         masterSting += CSVSting
         textField.text = masterSting
     }
@@ -89,6 +114,55 @@ class ListView: UIViewController {
         string = "\(sample.plot),\(sample.direction),\(sample.distance),\(sample.coverCheatgrass),\(sample.coverCheatgrassLitter),\(sample.coverGround),\(sample.coverLitter),\(sample.coverPlant)\n"
         return string
     }
+    
+    func contentToCSVString(content: storage.contents) -> String{
+        var string = ""
+        var value = 1
+        if content.contents == storage.contentTypes.Default{
+            value = 1
+        }else if content.contents == storage.contentTypes.UnburnedGround{
+            value = 2
+        }else if content.contents == storage.contentTypes.BurnedGround{
+            value = 3
+        }else if content.contents == storage.contentTypes.UnburnedBush{
+            value = 4
+        }else if content.contents == storage.contentTypes.AliveBurnedBush{
+            value = 5
+        }else if content.contents == storage.contentTypes.DeadBurnedBush{
+            value = 6
+        }else if content.contents == storage.contentTypes.ScorchedGround{
+            value = 7
+        }else if content.contents == storage.contentTypes.ScorchedBush{
+            value = 8
+        }
+        string = "\(content.plot),\(content.direction),\(content.distance),\(value)\n"
+        return string
+    }
+    
+    func contentToGoodString(content: storage.contents) -> String{
+        var string = ""
+        var value = "Default"
+        if content.contents == storage.contentTypes.Default{
+            value = "Default"
+        }else if content.contents == storage.contentTypes.UnburnedGround{
+            value = "Unburned Ground"
+        }else if content.contents == storage.contentTypes.BurnedGround{
+            value = "Burned Ground"
+        }else if content.contents == storage.contentTypes.UnburnedBush{
+            value = "Unburned Bush"
+        }else if content.contents == storage.contentTypes.AliveBurnedBush{
+            value = "Alive Burned Bush"
+        }else if content.contents == storage.contentTypes.DeadBurnedBush{
+            value = "Dead Burned Bush"
+        }else if content.contents == storage.contentTypes.ScorchedGround{
+            value = "Scorched Ground"
+        }else if content.contents == storage.contentTypes.ScorchedBush{
+            value = "Scorched Bush"
+        }
+        string = "Plot \(content.plot), direction \(content.direction), distance \(content.distance) contains \(value).\n"
+        return string
+    }
+    
     func pointToCSVString(point: storage.point) -> String{
         return "\(point.plot),\(point.direction),\(point.distance),\(point.row),\(point.column),\(point.cover)\n"
     }
@@ -148,6 +222,23 @@ class ListView: UIViewController {
         storage.listOfSamples = sampleList
         print("Figured Samples")
         print(sampleList)
+    }
+    
+    func contentExists(at: storage.contents) -> Int?{
+        let content = at
+        var iterations = 0
+        var match = false
+        for i in storage.listOfContents{
+            if i.plot == content.plot && i.direction == content.direction && i.distance == content.distance{
+                match = true
+                print("Found content at plot \(iterations).")
+                return iterations
+            }
+            iterations += 1
+        }
+        if match == false{
+            return nil
+        }
     }
     
     /*
